11aa7b45bae51ded050f60f8e63702f59cfbf836
unknown <ujjwalmishra21@gmail.com>
2021-02-27 20:36:35 +0530

    heroku error solve

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ujjwal'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity)    # /auth
 
